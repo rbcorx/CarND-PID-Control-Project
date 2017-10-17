9694bbe4bb8c958e7d97c93d5884d9af8704c245
Raunak Bhandari <ronniebhase@gmail.com>
2017-10-17 12:17:41 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -32,8 +32,13 @@ int main()
 {
   uWS::Hub h;
 
+  // Initialize the PID instances and its variables.
   PID pid;
-  // TODO: Initialize the pid variable.
+  double kp = 0.225;
+  double ki = 0.0; // No need to compensate for sytematic bias (we don't have them). 
+  double kd = 3.0;  
+  pid.Init(kp, ki, kd);
+
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -41,7 +46,7 @@ int main()
     // The 2 signifies a websocket event
     if (length && length > 2 && data[0] == '4' && data[1] == '2')
     {
-      auto s = hasData(std::string(data).substr(0, length));
+      auto s = hasData(std::string(data));
       if (s != "") {
         auto j = json::parse(s);
         std::string event = j[0].get<std::string>();
@@ -50,19 +55,20 @@ int main()
           double cte = std::stod(j[1]["cte"].get<std::string>());
           double speed = std::stod(j[1]["speed"].get<std::string>());
           double angle = std::stod(j[1]["steering_angle"].get<std::string>());
-          double steer_value;
           /*
           * TODO: Calcuate steering value here, remember the steering value is
           * [-1, 1].
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
+          pid.UpdateError(cte);
+          double steer_value = -pid.TotalError();
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
           json msgJson;
-          msgJson["steering_angle"] = steer_value;
+          msgJson["steering_angle"] = steer_value; // 
           msgJson["throttle"] = 0.3;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
