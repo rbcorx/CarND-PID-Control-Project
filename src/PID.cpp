65d07f6634d0036a274b785b282920debc0acc1c
Raunak Bhandari <ronniebhase@gmail.com>
2017-10-17 12:22:07 +0530

    Update PID.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -13,17 +13,14 @@ void PID::Init(double Kp, double Ki, double Kd) {
 	this->Ki = Ki; // Integral term 
 	this->Kd = Kd; // Differential term
 
-	// Init the Proportionnal error to a high value
-	// Because, at the very beginning, I assume the car is far from the CTE 
+	// Proportionnal error init to high value as car away from the CTE initially
 	p_error = numeric_limits<double>::max();  
 
-	// Init the Integral term to 0
-	// Because, at the very beginning, the total area (Integral definition) between the position of the car 
-	// and the CTE is null (because the car has not started yet)
+	// integral term init to 0 as initial total area (Integral definition) btw car pos & CTE is NULL 
+	// as car yet to start
 	i_error = 0.0;
 
-	// Init the Differential error to 0
-	// Because, at the very beginning, the position of the car is assumed to be far away from CTE
+	// differential error init ti 0 as initial car pos is assumed to away from CTE
 	d_error = 0.0; 
 }
 
